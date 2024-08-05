62f40857fee4feca6b39c84e674f3584d304d3fc
ankitsensei <main.ankitbhagat@gmail.com>
2024-08-05 16:25:06 +0530

    added wedding1 img

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import NavBar from './components/navbar';
 
 function App() {
     return (
-        <div className='bg-zinc-900 text-white w-full h-screen text-xl px-20'>
+        <div className='bg-zinc-900 text-white w-full h-full text-xl px-20'>
             <NavBar />
-            <div className='text-8xl font-MuseoModerno font-semibold tracking-tighter mt-32'>
-                <h1>YOU MAKE MEMORIES AND</h1>
-                <h1>LET US CAPTURE IT FOR YOU</h1>
+            <div className='w-full h-screen rounded-xl text-8xl  bg-wedding1 bg-cover'>
+                <div className='font-semibold tracking-tighter flex flex-col justify-center items-center h-screen'>
+                    <h1>YOU MAKE MEMORIES AND</h1>
+                    <h1>LET US CAPTURE IT FOR YOU</h1>
+                </div>
             </div>
         </div>
     );
