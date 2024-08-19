918d4c02032e859027cbb5e2f0f2959a4b9a9de9
ankitsensei <main.ankitbhagat@gmail.com>
2024-08-19 20:14:27 +0530

    changed the nav-bar right corner

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 
 function Navbar() {
-    const navLinks = {
-        Home: '/',
-        About: '/about',
-        Contact: '/contact',
-        'Book Now': '/book',
-    };
-
     return (
         <div className='flex justify-between text-white py-5'>
             <h1 className='text-3xl font-semibold'>CLASSYPICTURES</h1>
             <ul className='flex gap-10 text-xl'>
-                {Object.entries(navLinks).map(([key, value], index) => (
-                    <li key={index}>
-                        <a href={value}>{key}</a>
-                    </li>
-                ))}
+                <li>
+                    <a href='#'>HOME</a>
+                </li>
+                <li>
+                    <a href='#'>ABOUT</a>
+                </li>
+                <li>
+                    <a href='#'>CONTACT</a>
+                </li>
+                <li>
+                    <a href='#'>BOOK NOW</a>
+                </li>
             </ul>
         </div>
     );
