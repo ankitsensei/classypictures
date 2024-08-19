b02ea8988849377535670118560952e10663e9fe
ankitsensei <main.ankitbhagat@gmail.com>
2024-08-19 20:18:27 +0530

    navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Navbar() {
     return (
-        <div className='flex justify-between text-white py-5'>
+        <div className='flex justify-between items-center text-white py-5'>
             <h1 className='text-3xl font-semibold'>CLASSYPICTURES</h1>
-            <ul className='flex gap-10 text-xl'>
+            <ul className='flex gap-10 text-xl items-center'>
                 <li>
                     <a href='#'>HOME</a>
                 </li>
@@ -14,7 +14,7 @@ function Navbar() {
                 <li>
                     <a href='#'>CONTACT</a>
                 </li>
-                <li>
+                <li className='bg-red-600 w-40 h-10 flex items-center justify-center rounded-[20px]'>
                     <a href='#'>BOOK NOW</a>
                 </li>
             </ul>
