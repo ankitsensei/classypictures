e2cdd12a28ae25a9e5631be609467cefb4dd259a
ankitsensei <main.ankitbhagat@gmail.com>
2024-08-21 21:21:24 +0530

    added a gallery folder and added images to it

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -3,9 +3,8 @@ import Wed1 from './assets/pics/wed (1).jpg';
 import Wed2 from './assets/pics/wed (2).jpg';
 function Gallery() {
     return (
-        <div id='gallery' className='flex flex-col w-full'>
+        <div id='gallery' className='flex flex-col w-full '>
             <img src={Wed2} alt='wed2' className='rounded-[40px] ' />
-            
         </div>
     );
 }
