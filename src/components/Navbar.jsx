64fc859f613642a529c83d8ece1e9c8876e88a8d
ankitsensei <main.ankitbhagat@gmail.com>
2024-10-10 08:03:12 +0530

    navbar design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,14 +7,14 @@ const Navbar = () => {
             <div className='flex justify-between items-center mx-20 py-2'>
                 <div className='flex items-center gap-14'>
                     <img src={Logo} alt="logo" className='h-14'/>
-                    <ul className='flex gap-14'>
+                    <ul className='flex gap-10'>
                         <li>Home</li>
                         <li>Our Services</li>
                         <li>Gallery</li>
                         <li>Contact Us</li>
                     </ul>
                 </div>
-                <ul className='flex gap-8 items-center'>
+                <ul className='flex gap-3 items-center'>
                     <li className='border-2 border-white px-10 py-2 rounded-[20px]'>Join</li>
                     <li className='bg-white border-2 border-white text-black px-10 py-2 rounded-[20px]'>Book</li>
                 </ul>
